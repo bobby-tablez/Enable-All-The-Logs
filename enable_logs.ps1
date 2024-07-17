eff5b9c3a499f049b1a5ff74bd1c7f6c97ef05f9
Bobby-Tablez <git@pf5.co>
2024-07-17 13:24:47 -0600

    Update enable_logs.ps1

--- enable_logs.ps1
+++ enable_logs.ps1
@@ -17,18 +17,26 @@
 .PARAMETER -sysmononly 
     This will ONLY download and install sysmon. If sysmon is already present, it will perform a reinstall using the provided XML import config file.    
 .EXAMPLE 
-    enable_logs.ps1 -sysmononly
+    enable_logs.ps1 -sysmononly (-so)
 .PARAMETER -y 
     This will skip the "are you sure?" prompt upon initial execution.
 .EXAMPLE 
-    enable_logs.ps1 -y
+    enable_logs.ps1 -y (-yes)
     enable_logs.ps1 -y -sysmononly
 .PARAMETER -config
     Bring your own XML config file. When the -config argument is passed, supply a direct URL to a Sysmon config import file. When no argument is supplied it will download: "https://raw.githubusercontent.com/bobby-tablez/FT-Sysmon-Config/master/ft-sysmonconfig-export.xml"
 .EXAMPLE 
     enable_logs.ps1 -y
     enable_logs.ps1 -y -sysmononly
-    enable_logs.ps1 -y -sysmononly -config https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml
+    enable_logs.ps1 -yes -sysmononly -config https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml
+.PARAMETER -driver (-d)
+    Modify the Sysmon driver name. Limited to 8 characters.
+.EXAMPLE 
+    enable_logs.ps1 -y -driver "sccm"
+.PARAMETER -name (-n)
+    Modify the Sysmon binary file's name. The binary name will effectively become the service name. https://www.darkoperator.com/blog/2018/10/5/operating-offensively-against-sysmon
+.EXAMPLE 
+    enable_logs.ps1 -y -d "sccm" -name "sccm_service.exe"
 .COMPANYNAME
 
 .COPYRIGHT
@@ -51,27 +59,38 @@
 
 #>
 
-param(
+Param(
+    [Alias("so")]
     [switch]$sysmononly,
-    [switch]$y,
-    [string]$config
+
+    [Alias("y")]
+    [switch]$yes,
+
+    [Alias("c")]
+    [string]$config,
+
+    [Alias("d")]
+    [string]$driver,
+
+    [Alias("n")]
+    [string]$name
 )
 
 # Check for administrator privs
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host -ForegroundColor Red "Enable All The Logs! requires Administrator privileges. Please rerun in an admin PowerShell console."
     exit
 }
 
 # Bypass the warning prompt when -y arguement is supplied
-if (-Not $y){
-    if ($sysmononly){
+If (-Not $yes){
+    If ($sysmononly){
         $confirmation = $(Write-Host -f Yellow -NoNewLine "CAUTION: This script will download and install Sysmon. Do you want to continue? (y/n): "; Read-Host)
-    }else {
+    }Else {
         $confirmation = $(Write-Host -f Yellow -NoNewLine "CAUTION: This script will download and install Sysmon and make GPO and registry changes that will increase log volume. Continue? (y/n): "; Read-Host)
     }
 
-    if (-not($confirmation -eq 'y')) {
+    If (-not($confirmation -eq 'y')) {
         Write-Host "`nBye!"
         Exit
     }
@@ -86,83 +105,139 @@ $sysmonOut = "$env:temp\Sysmon.zip"
 $sysmonConfOut = "$env:temp\sysmon-config.xml"
 
 # Use user-supplied config import file if -config arguement is supplied
-if ($config) {
+If ($config) {
     $sysmonConf = $config
-} else {
+} Else {
     $sysmonConf = "https://raw.githubusercontent.com/bobby-tablez/FT-Sysmon-Config/master/ft-sysmonconfig-export.xml"
+}
+
+# Grab the new driver name, check that the length does not exceed 8 characters
+Try {
+    if ($driver) {
+        if ($driver.Length -le 8) {
+            $driver = "-d $driver"
+        } else {
+            Write-Host "[ " -nonewline; Write-Host $ex -f red -nonewline; Write-Host "Error: Driver string must be 8 characters or less."
+            exit 1
+        }
+    }
+} Catch {
+    Write-Host "An unexpected error occurred: $_"
+    Exit 1
 }
 
 # Begin actions
 Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Downloading Sysmon"
-try { 
+Try { 
     Invoke-WebRequest -URI $sysmonURL -OutFile $sysmonOut
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Sysmon downloaded"
-} catch {
+} Catch {
     $errorSysmon = $_.Exception.Message
     Write-Host "[ " -nonewline; Write-Host $ex -f red -nonewline; Write-Host " ] Error occurred while downloading Sysmon: $errorSysmon"
-    exit 1
+    Exit 1
 }
 
 # Sysmon XML file
 Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Downloading Sysmon config import file"
-try { 
+Try { 
     Invoke-WebRequest -URI $sysmonConf -OutFile $sysmonConfOut
     # Attempt to load the file as an XML to validate its content
     [xml]$xmlContent = Get-Content -Path $sysmonConfOut
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Import config file downloaded and validated as XML"
-} catch {
+} Catch {
     $errorXML = $_.Exception.Message
-    if ($_ -is [System.Xml.XmlException]) {
+    If ($_ -is [System.Xml.XmlException]) {
         Write-Host "[ " -nonewline; Write-Host $ex -f red -nonewline; Write-Host " ] The downloaded config import file is not valid XML: $errorXML"
         exit 1 
-    } else {
+    } Else {
         Write-Host "[ " -nonewline; Write-Host $ex -f red -nonewline; Write-Host " ] Error occurred while downloading the config import file: $errorXML"
-        exit 1
+        Exit 1
     }
 }
 
 # Extract Sysmon archive contents into %TEMP%
 Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Extracting Sysmon archive"
-try { 
+Try { 
     Expand-Archive $sysmonOut -Destination $env:temp -ErrorAction Stop -Force
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Sysmon archive extracted"
-} catch {
+} Catch {
     $errorZIP = $_.Exception.Message
     Write-Host "[ " -nonewline; Write-Host $ex -f red -nonewline; Write-Host " ] Error occurred while extracting the Sysmon archive: $errorZIP"
-    exit 1
+    Exit 1
 }
 
+Function Rename-Sysmon {
+    Param (
+        [string]$sourcePath,
+        [string]$destinationPath
+    )
+    
+    If (Test-Path $sourcePath) {
+        Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Renaming binary to $destinationPath"
+        Rename-Item -Path $sourcePath -NewName $destinationPath
+    } Else {
+        Write-Host "[ " -nonewline; Write-Host "ERROR" -f red -nonewline; Write-Host " ] Source binary not found: $sourcePath"
+        Exit 1
+    }
+}
+
+# Used to uninstall sysmon prior to (re)installation
+Function Uninstall-Sysmon {
+    Param (
+        [string]$serviceName,
+        [string]$exePath
+    )
+
+    $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+    if ($service.Length -gt 0) {
+        Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Uninstalling Existing version of $serviceName"
+        Start-Process -FilePath $exePath -ArgumentList "-u" -Wait
+    }
+}
+
+# Get CPU arch
 Function b64{
     $b64test = [Environment]::Is64BitOperatingSystem #Used to install x86/x64 Sysmon per OS architecture
     return $b64test
 }
 
-if (b64) { 
-    # Uninstall if present (64-bit)
-    $service64 = Get-Service -Name Sysmon64 -ErrorAction SilentlyContinue
-    if ($service64.Length -gt 0) {
-        Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Uninstalling Existing version of Sysmon64"
-        Start-Process "Sysmon64.exe" -ArgumentList "-u" -Wait
-    }
-    # Install Sysmon (64-bit)
-    write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Installing Sysmon64"
-    Start-Process -FilePath "$env:temp\Sysmon64.exe" -ArgumentList "-accepteula -i $sysmonConfOut" -Wait
+# Set veriables of service and file names based on cpu arch
+If (b64) {
+    $originalExePath = "$env:temp\Sysmon64.exe"
+    $ServiceName = "Sysmon64"
+} Else {
+    $originalExePath = "$env:temp\Sysmon.exe"
+    $ServiceName = "Sysmon"
 }
-else {
-    # Uninstall if present (32-bit)
-    $service = Get-Service -Name Sysmon -ErrorAction SilentlyContinue
-    if ($service.Length -gt 0) {
-        Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Uninstalling Existing version of Sysmon"
-        Start-Process "Sysmon.exe" -ArgumentList "-u" -Wait
-    }
-    # Install Sysmon (32-bit)
-    write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Installing Sysmon"
-    Start-Process -FilePath "$env:temp\Sysmon.exe" -ArgumentList "-accepteula -i $sysmonConfOut" -Wait
+
+If ($name) {
+    $SysmonExt = "$name.exe"
+    $newExePath = "$env:temp\$SysmonExt"
+    Rename-Sysmon -sourcePath $originalExePath -destinationPath $newExePath
+    $sysmonExe = $newExePath
+} Else {
+    $sysmonExe = $originalExePath
 }
+
+# Uninstall Sysmon if present (consideration for original and/or renamed binaries)
+Uninstall-Sysmon -serviceName $ServiceName -exePath $originalExePath
+If ($name) {
+    Uninstall-Sysmon -serviceName $ServiceName -exePath $sysmonExe
+}
+
+# Install Sysmon
+If ($sysmononly) {
+    Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Installing Sysmon64"
+} Else {
+    Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Installing Sysmon"
+}
+Start-Process -FilePath $sysmonExe -ArgumentList "-accepteula -i $config $driver" -Wait
+
 Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Sysmon successfully installed"
 Write-Host ""
 
-if (-Not $sysmononly){
+# Begin non-Sysmon related tasks (unless -sysmononly is supplied)
+If (-Not $sysmononly){
 
     # PowerShell logging registry changes
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Enabling PowerShell scriptblock logging"
@@ -170,10 +245,10 @@ if (-Not $sysmononly){
     $PSregValName = "EnableScriptBlockLogging"
     $PSregValDat = 1
 
-    if (-not (Test-Path $PSregPath)) {
+    If (-not (Test-Path $PSregPath)) {
         New-Item -Path $PSregPath -ItemType Directory -Force  | Out-Null
     }
-    Set-ItemProperty -Path $PSregPath -Name $PSregValName -Value $PSregValDat -Type DWord  | Out-Null
+    Set-ItemProperty -Path $PSregPath -Name $PSregValName -Value $PSregValDat -Type DWord | Out-Null
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] PowerShell Script Block Logging enabled"
 
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Enabling PowerShell module logging"
@@ -181,16 +256,16 @@ if (-Not $sysmononly){
     $PSMregValName = "EnableModuleLogging"
     $PSMregValDat = 1
 
-    if (-not (Test-Path $PSMregPath)) {
+    If (-not (Test-Path $PSMregPath)) {
         New-Item -Path $PSMregPath -ItemType Directory -Force  | Out-Null
     }
-    Set-ItemProperty -Path $PSMregPath -Name $PSMregValName -Value $PSMregValDat -Type DWord  | Out-Null
+    Set-ItemProperty -Path $PSMregPath -Name $PSMregValName -Value $PSMregValDat -Type DWord | Out-Null
 
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] PowerShell Module Logging enabled"
 
     # Applies defined policies in $auditSettings
-    function ApplyPolicy {
-        param (
+    Function ApplyPolicy {
+        Param (
             [Parameter(Mandatory)]
             [string]$subcategory,
 
@@ -206,12 +281,12 @@ if (-Not $sysmononly){
     }
 
     # Used to enable CommandLine w/4688
-    function ProcStartCMD {
+    Function ProcStartCMD {
         $cmdRegPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit"
         $cmdRegValue = "ProcessCreationIncludeCmdLine_Enabled"
         $cmdRegData = 1
 
-        if (-not (Test-Path $cmdRegPath)) {
+        If (-not (Test-Path $cmdRegPath)) {
             New-Item -Path $cmdRegPath -ItemType Directory -Force
         }
         Set-ItemProperty -Path $cmdRegPath -Name $cmdRegValue -Value $cmdRegData -Type DWord
@@ -298,7 +373,7 @@ if (-Not $sysmononly){
     }
 
     # Apply each policy
-    foreach ($policy in $auditSettings.Keys) {
+    Foreach ($policy in $auditSettings.Keys) {
         $settings = $auditSettings[$policy]
         ApplyPolicy -subcategory $policy -success $settings.Success -failure $settings.Failure
     }
@@ -313,8 +388,10 @@ if (-Not $sysmononly){
     Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Updating GPOs using gpupdate"
     Invoke-Expression -Command "gpupdate /force" | Out-Null
 
-    # Clean Up
-    Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Cleaning up"
-    Remove-Item $sysmonOut,$sysmonConfOut,$env:temp\Sysmon.exe,$env:temp\sysmon64.exe,$env:temp\sysmon64a.exe
-};
+}
+
+# Clean Up
+Write-Host "[ " -nonewline; Write-Host $cm -f green -nonewline; Write-Host " ] Cleaning up"
+Remove-Item $sysmonOut,$sysmonConfOut,$sysmonExe,$env:temp\sysmon64.exe,$env:temp\sysmon64a.exe -ErrorAction SilentlyContinue | Out-Null
+
 Write-Host -f green "`nDone!"
